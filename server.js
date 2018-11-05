25d58301741eaa3854d86380425f2da2b7733af4
ChienNguyen <cnguyen1494@gmail.com>
2018-11-05 12:12:08 -0600

    testing 2

--- server.js
+++ server.js
@@ -8,30 +8,12 @@ app.use(express.static(__dirname + '/public_html'));
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-hbs.registerHelper('getCurrentYear',()=>{
-    return new Date().getFullYear();
-});
-
-app.use((req,res,next) =>{
-    var now = new Date().toString();
-    var log = `${now}: ${req.method} ${req.url}`
-    console.log(log);
-    fs.appendFile('server.log', log +'\n', (err) =>{
-        if(err){console.log('Unable to append to server.log');}
-    });
-    next();
-});
-/*
-app.use((req,res,next) =>{
-    res.render('maintenance.hbs');
-});
-*/
 
 
 
 app.get('/',(req,res) =>{
     //res.send('<h1>hello express</h1>');
-    res.render('home.hbs');
+    res.render('index.hbs');
 });
 
 
