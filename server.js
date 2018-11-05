b8ba37b552d37e575d9d245a0d3c7dfb7e963a3c
ChienNguyen <cnguyen1494@gmail.com>
2018-11-05 12:10:30 -0600

    fixed

--- server.js
+++ server.js
@@ -3,27 +3,38 @@ const hbs = require('hbs');
 const fs = require('fs');
 var app = express();
 
-const port = process.env.port || 3000;
-
+const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public_html'));
-hbs.registerPartials(__dirname + '/public_html/partials');
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
 
 app.use((req,res,next) =>{
     var now = new Date().toString();
-    var log = `${now}: ${req.method} ${req.url}`;
+    var log = `${now}: ${req.method} ${req.url}`
     console.log(log);
     fs.appendFile('server.log', log +'\n', (err) =>{
         if(err){console.log('Unable to append to server.log');}
     });
     next();
 });
+/*
+app.use((req,res,next) =>{
+    res.render('maintenance.hbs');
+});
+*/
+
 
 
 app.get('/',(req,res) =>{
+    //res.send('<h1>hello express</h1>');
     res.render('home.hbs');
 });
 
-app.listen(port, () => {
-    console.log(`Server started on ${port}`);
+
+app.listen(port,()=>{
+    console.log(`Server is on ${port}`);
 });
